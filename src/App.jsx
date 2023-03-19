db75e5108a03d37a8623a2957fa5de90fbdde0c4
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2023-03-19 21:01:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/jsx-no-useless-fragment */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable no-unused-vars */
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
